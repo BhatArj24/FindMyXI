74829e652d13e985d6c1bbb81d8d39dda6fe94cd
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-03-31 13:41:54 -0700

    m

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import circleLogo from '../Images/CircleLogo.png'
 import { useNavigate } from 'react-router-dom';
 
