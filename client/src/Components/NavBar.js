238c20ca60a0991d46420b95eb969cdbf4958087
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-03-31 13:36:51 -0700

    m

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
-// import {circleLogo} from './Images/'
+import circleLogo from '../Images/CircleLogo.png'
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+    const navigate = useNavigate();
     return(
         <header>
             <nav className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
-                <h1 className="h3 my-0 me-md-auto fw-bold text-white">FindMy<span>XI</span></h1>
+                <img src={circleLogo} alt="FindMyXI Logo" className="my-0 me-md-auto" style={{width:"50px"}} onClick={()=>navigate('/home')} />
                 <nav className="my-2 my-md-0 me-md-3">
                     <Link className="mx-2 btn btn-sm p-2 text-light btn-dark" to="/home">Home</Link>                  
                     <Link className="mx-2 btn btn-sm p-2 text-light btn-dark" to="/browse">Browse</Link>
