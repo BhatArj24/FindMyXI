1dbda80f6f12f7bda31f1399c75d6b7784387793
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-16 18:08:26 -0700

    server

--- server/routes/reset.js
+++ server/routes/reset.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const {User} = require('../models/user');
 
 router.get("/:email", async (req, res) => {
-    const user = await User.find({ email: req.params.email});
+  const userEmail = req.params.email;
+    const user = await User.find({ email: userEmail});
 
   return res.status(200).send({ data: user._id });
 });
