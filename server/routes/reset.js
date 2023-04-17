420df52d29b868b9bdf95f0840bd2edda5dde702
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-16 18:12:25 -0700

    server

--- server/routes/reset.js
+++ server/routes/reset.js
@@ -3,9 +3,17 @@ const {User} = require('../models/user');
 
 router.get("/:email", async (req, res) => {
   const userEmail = req.params.email;
-    const user = await User.find({ email: userEmail});
+    // const user = await User.find({ email: userEmail});
+    User.find({ email: userEmail })
+    .then(user => {
+      res.status(200).send({ data: user._id });
+    })
+    .catch(err => {
 
-  return res.status(200).send({ data: user._id });
+      res.status(500).send({ message: "No account under this email" });
+    })
+
+
 });
 
 module.exports = router;
