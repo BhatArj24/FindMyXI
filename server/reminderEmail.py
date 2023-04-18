8d25a777d9b012b1ff7dbc75bac313be4d5f2d74
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-17 17:10:29 -0700

    reminder email

---
+++ server/reminderEmail.py
@@ -0,0 +1,22 @@
+
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os
+
+
+load_dotenv(find_dotenv())
+password = os.environ.get("MONGODB_PWD")
+connection_string = f"mongodb+srv://arjun:{password}@findmyxicluster.dcwfpzh.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(connection_string)
+db = client["FindMyXIData"]
+collection = db["users"]
+
+
+emails = []
+for doc in collection.find({"isPlayer": True}):
+    if 'email' in doc:
+        emails.append(doc['email'])
+
+for email in emails:
+    print(email)
