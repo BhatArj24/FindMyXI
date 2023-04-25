c7cf752a1af263868cc9561915c6639e4248d99e
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-24 19:20:56 -0700

    reverting players api call

--- server/routes/players.js
+++ server/routes/players.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const {User} = require('../models/user');
 
 router.get("/", async (req, res) => {
-  const skip = req.query.skip ? Number(req.query.skip) : 0;
-	const DEFAULT_LIMIT = 8;
-    const users = await User.find({ isPlayer: true}).skip(skip).limit(DEFAULT_LIMIT);
+    const users = await User.find({ isPlayer: true});
 
   return res.status(200).send({ data: users });
 });
