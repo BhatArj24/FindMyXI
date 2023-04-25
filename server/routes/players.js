97b7497411b0e332ccf88ad4b874bdb081dc37ed
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-24 18:25:44 -0700

    infinite scrolling

--- server/routes/players.js
+++ server/routes/players.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const {User} = require('../models/user');
 
 router.get("/", async (req, res) => {
-    const users = await User.find({ isPlayer: true});
+  const skip = req.query.skip ? Number(req.query.skip) : 0;
+	const DEFAULT_LIMIT = 8;
+    const users = await User.find({ isPlayer: true}).skip(skip).limit(DEFAULT_LIMIT);
 
   return res.status(200).send({ data: users });
 });
