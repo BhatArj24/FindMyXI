f0d9c863eb9f3b59ea7e9ae23491fadd0c6d4b25
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-24 19:23:54 -0700

    reverting browse page

---
+++ FileChanges/Browse.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "../NavBar";
+import UserCard from "./UserCard";
+import "./Card.css";
+import axios from "axios";
+import "./Browse.css";
+import "./Loader.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const Browse = () => {
+  const [players, setPlayers] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+  const [filterType, setFilterType] = useState("role");
+  const [isLoading, setIsLoading] = useState(true);
+  const [availableSaturday, setAvailableSaturday] = useState(false);
+  const [availableSunday, setAvailableSunday] = useState(false);
+  const [skip, setSkip] = useState(0);
+  const [isEnd, setIsEnd] = useState(false);
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+  const getPlayers = async () => {
+    setSkip(players?.length);
+    const url = `https://findmyxi.onrender.com/api/players?skip=${skip}`;
+    setIsLoading(true);
+    try {
+      const res = await axios.get(url);
+      if (res.data.data.length < 8) {
+        setPlayers([...players, ...res.data.data]);
+        setIsEnd(true);
+        return;
+      }
+      if(players) {
+
+      setPlayers([...players, ...res.data.data]);
+      }
+      else {
+      setPlayers(res.data.data);
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, [skip]);
+  const fetchPlayers = async () => {
+    await getPlayers();
+  };
+
+  const renderUsers = (
+    players &&
+    players
+      .filter((player) =>
+        filterType === "name"
+          ? player.name.toLowerCase().includes(searchValue.toLowerCase())
+          : filterType === "role"
+          ? player.role.toLowerCase().includes(searchValue.toLowerCase())
+          : player.primaryTeam.toLowerCase().includes(searchValue.toLowerCase())
+      )
+      .filter((player) => {
+        if (availableSaturday && availableSunday) {
+          return player.availableSat === true && player.availableSun === true;
+        } else if (availableSaturday) {
+          return player.availableSat === true;
+        } else if (availableSunday) {
+          return player.availableSun === true;
+        } else {
+          return true;
+        }
+      })
+      .map((player) => <UserCard key={player._id} player={player} />)
+  );
+
+  return (
+    <section>
+      <NavBar />
+      <div className="flex flex-col lg:flex-row">
+        <div className="boxContainer">
+          <table className="elementsContainer">
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  placeholder="Fliter by..."
+                  className="search"
+                  value={searchValue}
+                  onChange={handleSearchChange}
+                ></input>
+              </td>
+
+              <td>
+                <select
+                  className="filter"
+                  value={filterType}
+                  onChange={(e) => {
+                    setFilterType(e.target.value);
+                  }}
+                >
+                  <option className="option" value="role">
+                    Role
+                  </option>
+                  <option className="option" value="name">
+                    Name
+                  </option>
+                  <option className="option" value="primaryTeam">
+                    Primary Team
+                  </option>
+                </select>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div className="bg-blue-700 text-white rounded-md p-3 mt-2 mx-3 lg:mr-8">
+          <div>
+            <input
+              type="checkbox"
+              checked={availableSaturday}
+              onChange={(e) => setAvailableSaturday(e.target.checked)}
+              className="mr-2"
+            />
+            <label htmlFor="availableSaturday">Available Saturday</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              checked={availableSunday}
+              onChange={(e) => setAvailableSunday(e.target.checked)}
+              className="mr-2"
+            />
+            <label htmlFor="availableSunday">Available Sunday</label>
+          </div>
+        </div>
+      </div>
+      <InfiniteScroll
+        className="flex-wrap flex flex-col w-full mt-2 lg:flex-row"
+        dataLength={players ? players.length : 0}
+        next={fetchPlayers}
+        hasMore={!isEnd}
+        loader={<div className="spinner"></div>}
+      >
+        {renderUsers}
+      </InfiniteScroll>
+    </section>
+  );
+};
+
+export default Browse;
--- client/src/Components/Browse/Browse.js
+++ client/src/Components/Browse/Browse.js
@@ -1,153 +1,139 @@
-import React, { useState, useEffect } from "react";
-import NavBar from "../NavBar";
-import UserCard from "./UserCard";
-import "./Card.css";
-import axios from "axios";
-import "./Browse.css";
-import "./Loader.css";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, {useState,useEffect} from 'react';
+import NavBar from '../NavBar';
+import UserCard from './UserCard';
+import './Card.css';
+import {BsSearch} from 'react-icons/bs';
+import axios from 'axios';
+import './Browse.css';
+import './Loader.css';
 
 const Browse = () => {
-  const [players, setPlayers] = useState(null);
-  const [searchValue, setSearchValue] = useState("");
-  const [filterType, setFilterType] = useState("role");
-  const [isLoading, setIsLoading] = useState(true);
-  const [availableSaturday, setAvailableSaturday] = useState(false);
-  const [availableSunday, setAvailableSunday] = useState(false);
-  const [skip, setSkip] = useState(0);
-  const [isEnd, setIsEnd] = useState(false);
+    const [players,setPlayers] = useState();
+    const [searchValue, setSearchValue] = useState("");
+    const [filterType, setFilterType] = useState("role");
+    const [isLoading, setIsLoading] = useState(true);
+    const [availableSaturday, setAvailableSaturday] = useState(false);
+const [availableSunday, setAvailableSunday] = useState(false);
 
-  const handleSearchChange = (e) => {
-    setSearchValue(e.target.value);
-  };
-  const getPlayers = async () => {
-    setSkip(players?.length);
-    const url = `https://findmyxi.onrender.com/api/players?skip=${skip}`;
-    setIsLoading(true);
-    try {
-      const res = await axios.get(url);
-      if (res.data.data.length < 8) {
-        setPlayers([...players, ...res.data.data]);
-        setIsEnd(true);
-        return;
-      }
-      if(players) {
-
-      setPlayers([...players, ...res.data.data]);
-      }
-      else {
-      setPlayers(res.data.data);
-      }
-
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchPlayers();
-  }, [skip]);
-  const fetchPlayers = async () => {
-    await getPlayers();
-  };
-
-  const renderUsers = (
-    players &&
-    players
-      .filter((player) =>
-        filterType === "name"
-          ? player.name.toLowerCase().includes(searchValue.toLowerCase())
-          : filterType === "role"
-          ? player.role.toLowerCase().includes(searchValue.toLowerCase())
-          : player.primaryTeam.toLowerCase().includes(searchValue.toLowerCase())
-      )
-      .filter((player) => {
-        if (availableSaturday && availableSunday) {
-          return player.availableSat === true && player.availableSun === true;
-        } else if (availableSaturday) {
-          return player.availableSat === true;
-        } else if (availableSunday) {
-          return player.availableSun === true;
-        } else {
-          return true;
+    const handleSearchChange = (e) => {
+        setSearchValue(e.target.value);
+    };
+    const getPlayers = async () => {
+        const url = "https://findmyxi.onrender.com/api/players";
+        setIsLoading(true);
+        try{
+            console.log("fetching players: "+window.performance.now());
+            const res = await axios.get(url);
+            console.log("fetching players: "+window.performance.now());
+            setPlayers(res.data.data);
+            setIsLoading(false);
+            
+        } catch(error){
+          console.log(error);
+          setIsLoading(false);
         }
-      })
-      .map((player) => <UserCard key={player._id} player={player} />)
-  );
+      };
+    
+    useEffect(()=>{
+        fetchPlayers();
+    },[]);
+    const fetchPlayers = async () => {
+        await getPlayers();
+    };
 
-  return (
-    <section>
-      <NavBar />
-      <div className="flex flex-col lg:flex-row">
-        <div className="boxContainer">
-          <table className="elementsContainer">
-            <tr>
-              <td>
-                <input
-                  type="text"
-                  placeholder="Fliter by..."
-                  className="search"
-                  value={searchValue}
-                  onChange={handleSearchChange}
-                ></input>
-              </td>
+    const renderUsers = isLoading ? (
+        <div className="spinner"></div>
+    ) : (
+        players &&
+        players
+          .filter((player) =>
+            filterType === "name"
+              ? player.name.toLowerCase().includes(searchValue.toLowerCase())
+              : filterType === "role"
+              ? player.role.toLowerCase().includes(searchValue.toLowerCase())
+              : player.primaryTeam.toLowerCase().includes(searchValue.toLowerCase())
+          )
+          .filter((player) => {
+            if (availableSaturday && availableSunday) {
+                return player.availableSat ===true && player.availableSun ===true;
+            } else if (availableSaturday) {
+                return player.availableSat ===true;
+            } else if (availableSunday) {
+                return player.availableSun ===true;
+            } else {
+                return true;
+            }
+        })
+          .map((player) => <UserCard key={player._id} player={player} />)
+    );
+    
+    return (
+      <section>
+        <NavBar />
+        <div className="flex flex-col lg:flex-row">
+          <div className="boxContainer">
+            <table className="elementsContainer">
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    placeholder="Fliter by..."
+                    className="search"
+                    value={searchValue}
+                    onChange={handleSearchChange}
+                  ></input>
+                </td>
 
-              <td>
-                <select
-                  className="filter"
-                  value={filterType}
-                  onChange={(e) => {
-                    setFilterType(e.target.value);
-                  }}
-                >
-                  <option className="option" value="role">
-                    Role
-                  </option>
-                  <option className="option" value="name">
-                    Name
-                  </option>
-                  <option className="option" value="primaryTeam">
-                    Primary Team
-                  </option>
-                </select>
-              </td>
-            </tr>
-          </table>
-        </div>
-        <div className="bg-blue-700 text-white rounded-md p-3 mt-2 mx-3 lg:mr-8">
-          <div>
-            <input
-              type="checkbox"
-              checked={availableSaturday}
-              onChange={(e) => setAvailableSaturday(e.target.checked)}
-              className="mr-2"
-            />
-            <label htmlFor="availableSaturday">Available Saturday</label>
+                <td>
+                  <select
+                    className="filter"
+                    value={filterType}
+                    onChange={(e) => {
+                      setFilterType(e.target.value);
+                    }}
+                  >
+                    <option className="option" value="role">
+                      Role
+                    </option>
+                    <option className="option" value="name">
+                      Name
+                    </option>
+                    <option className="option" value="primaryTeam">
+                      Primary Team
+                    </option>
+                  </select>
+                </td>
+              </tr>
+            </table>
           </div>
-          <div>
-            <input
-              type="checkbox"
-              checked={availableSunday}
-              onChange={(e) => setAvailableSunday(e.target.checked)}
-              className="mr-2"
-            />
-            <label htmlFor="availableSunday">Available Sunday</label>
+          <div className='bg-blue-700 text-white rounded-md p-3 mt-2 mx-3 lg:mr-8'>
+            <div>
+              <input
+                type="checkbox"
+                checked={availableSaturday}
+                onChange={(e) => setAvailableSaturday(e.target.checked)}
+                className="mr-2"
+              />
+              <label htmlFor="availableSaturday">Available Saturday</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                checked={availableSunday}
+                onChange={(e) => setAvailableSunday(e.target.checked)}
+                className="mr-2"
+
+              />
+              <label htmlFor="availableSunday">Available Sunday</label>
+            </div>
           </div>
         </div>
-      </div>
-      <InfiniteScroll
-        className="flex-wrap flex flex-col w-full mt-2 lg:flex-row"
-        dataLength={players ? players.length : 0}
-        next={fetchPlayers}
-        hasMore={!isEnd}
-        loader={<div className="spinner"></div>}
-      >
-        {renderUsers}
-      </InfiniteScroll>
-    </section>
-  );
+        <div className="flex-wrap flex flex-col w-full mt-2 lg:flex-row">
+          {renderUsers}
+        </div>
+      </section>
+    );
 };
+
 
 export default Browse;
